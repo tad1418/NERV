26cc1c969128cba77ef95269595df69c4441ce33
tad1418 <anhdat27102009@gmail.com>
2025-10-29 10:29:12 +0700

    Update customize.sh

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -34,7 +34,7 @@ fi
 
 # Set custom Display ID prop
 STOCK_PROP="$(GET_PROP "system" "ro.build.display.id")"
-CUSTOM_PROP="Project NERV $(echo -n ${ROM_VERSION} | cut -d "-" -f1)-${ROM_CODENAME} - ${TARGET_CODENAME} [${STOCK_PROP}]"
+CUSTOM_PROP="ArkaOS $(echo -n ${ROM_VERSION} | cut -d "-" -f1)-${ROM_CODENAME} - ${TARGET_CODENAME} [${STOCK_PROP}]"
 SET_PROP "system" "ro.build.display.id" "$CUSTOM_PROP"
 
 # Crok's RAM Managment Fix
